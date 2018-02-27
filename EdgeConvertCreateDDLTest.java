b07175d7d11b3bf80623c2bf713a8d98a119805b
Louis Trapani <lxt2528@rit.edu>
2018-02-27 11:30:22 -0500

    Implemented testGetField and testGetTable. Both passed JUnit test.

--- EdgeConvertCreateDDLTest.java
+++ EdgeConvertCreateDDLTest.java
@@ -1,6 +1,8 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class EdgeConvertCreateDDLTest extends EdgeConvertCreateDDLTestClass {
     EdgeConvertCreateDDLTestClass testObj;
@@ -16,45 +18,32 @@ public class EdgeConvertCreateDDLTest extends EdgeConvertCreateDDLTestClass {
         EdgeTable[] tblArr = {tbl};
         EdgeField[] fldArr = {fld};
         testObj = new EdgeConvertCreateDDLTestClass(tblArr, fldArr);
-        //super();
-        //EdgeTable tbl = new EdgeTable("2|anotherTest");
-        //EdgeField fld = new EdgeField("1|test");
-        //super(new EdgeTable[]{new EdgeTable("2|anotherTest")}, new EdgeField[]{new EdgeField("1|test")});
-//        EdgeTable tbl = new EdgeTable("1|test");
-//        EdgeField fld = new EdgeField("1|anotherTest");
     }
 
     @Before
     public void setUp() throws Exception {
-        testObj = new EdgeConvertCreateDDLTest();
         runner();
     }
 
     public void runner() {
-        //testInitialize();
-        //testGetTable();
+        testInitialize();
+        testGetTable();
         testGetField();
     }
 
-
     @Test
     public void testInitialize() {
+        assertEquals(tbl, testObj.getTable(2));
     }
 
     @Test
     public void testGetTable() {
+        assertEquals(tbl, testObj.getTable(2));
     }
 
     @Test
     public void testGetField() {
-//        EdgeField[] tempFields = new  EdgeField[2];
-//        EdgeField temp1 =  new EdgeField("1|testName");
-//        EdgeField temp2 = new EdgeField("2|testName2");
-//        tempFields[0] = temp1;
-//        tempFields[1] = temp2;
-//
-//        assertEquals("GetField should return ", tempFields[0], testObj.getField(1));
-
+        assertEquals(fld, testObj.getField(1));
     }
 
 
